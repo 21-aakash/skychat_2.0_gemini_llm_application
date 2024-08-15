773d7b40016da4923a089f8ff2e2ef1c579fd703
Aakash Tembhare <82498272+21-aakash@users.noreply.github.com>
2024-08-15 12:39:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+import time
+import streamlit as st
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+from streamlit_chat import message
+
+# Load environment variables
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Function to load Gemini Pro model and get responses
+model = genai.GenerativeModel("gemini-pro")
+chat = model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response = chat.send_message(question, stream=True)
+    return response
+
+# Initialize the Streamlit app
+st.set_page_config(page_title="Project: Q&A Chatbot")
+
+# Add custom CSS to set border and font
+st.markdown(
+    """
+    <style>
+    .main {
+        height:auto;
+        width:50%;
+        margin:auto;
+        border: 2px solid #007BFF; /* Blue border */
+        padding: 10px;
+        border-radius: 8px;
+        max-height: 80vh; /* Maximum height to allow scrolling */
+        overflow-y: auto; /* Enable vertical scrolling */
+        font-family: 'Arial', sans-serif; /* Change font family */
+    }
+    .streamlit-expanderHeader {
+        font-family: 'Arial', sans-serif;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.header("SkyChat 2.0.0 : gemini version(history)")
+
+# Initialize session state for chat history if it doesn't exist
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+
+input = st.text_input("Input: ", key="input")
+submit = st.button("Ask the me :)")
+
+if submit and input:
+    # Show a spinner while waiting for the response
+    with st.spinner("Generating response..."):
+      
+        response = get_gemini_response(input)
+        # Add user query and response to session state chat history
+        st.session_state['chat_history'].append(("You", input))
+        for chunk in response:
+            st.session_state['chat_history'].append(("Bot", chunk.text))
+        st.success("Response generated successfully!", icon="✅")
+
+# Display the chat history using the streamlit_chat message function
+for role, text in reversed(st.session_state['chat_history']):
+    if role == "You":
+        message(text, is_user=True)
+    else:
+        message(text)
